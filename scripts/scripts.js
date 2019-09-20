cf82c69293da428831b71c94ecfce0e43a41503f
Trukken <tomolya12@gmail.com>
2019-09-20 09:26:36 +0200

    Cuby moving

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+let cuby = document.querySelector(".cuby");
+let cubyPosX = 0;
+let cubyPosY = 0;
+document.addEventListener("keyup", function (e) {
+    console.log(e);
+    move(e);
+});
+
+function move(e) {
+    console.log(cubyPosX, cubyPosY);
+    switch (e.keyCode) {
+        case 40:
+            console.log(40);
+            cubyPosX += 1;
+            break;
+        case 37:
+            console.log(37);
+            cubyPosY += -1;
+            break;
+        case 38:
+            console.log(38);
+            cubyPosX += -1;
+            break;
+        case 39:
+            console.log(39);
+            cubyPosY += 1;
+            break;
+    }
+    let pozX = cubyPosX + "%";
+    let pozY = cubyPosY + "%";
+    cuby.style.top = pozX;
+    cuby.style.left = pozY;
+    console.log(pozX, pozY);
+}
